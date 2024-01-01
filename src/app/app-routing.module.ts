68589d5169cf6d2a8128ed3983d55a269ef1ac45
ganeshkale07 <ggkale07@gmail.com>
2024-01-01 20:42:01 +0530

    Some responsiveness issue fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { ContactComponent } from "./contact/contact.component";
+import {MatIconModule} from '@angular/material/icon';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [{
+  path:'about',
+  component:AboutComponent
+},{
+  path:'experience',
+  component:ExperienceComponent
+},{
+  path:'projects',
+  component:ExperienceComponent
+},{
+  path:'skills',
+  component:ExperienceComponent
+},{
+  path : '',
+  redirectTo : 'about',
+  pathMatch:"full"
+},{
+  path : '**',
+  component : ContactComponent
+}];
+
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,routerOptions),MatIconModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+  isMobileView : boolean = true;
+  constructor(@Inject(DOCUMENT) private document: Document){
+
+  }
+  ngOnInit(){
+  }
+
+  hambugerMenuClick(){
+    this.isMobileView = !this.isMobileView;
+  }
+
+
 }
